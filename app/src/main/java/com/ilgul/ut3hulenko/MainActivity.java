8d0142714bf859f1b4dad535f5de09da6bd94e2f
Illia Hulenko <ihulenk1@educacion.navarra.es>
2021-12-18 17:30:01 +0100

    UT3 homework done

---
+++ app/src/main/java/com/ilgul/ut3hulenko/MainActivity.java
@@ -0,0 +1,23 @@
+package com.ilgul.ut3hulenko;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    Locale locale;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+    public void setLocale(){
+
+    }
+}
---
+++ app/src/main/java/com/ilgul/ut3hulenko/Splashscreen.java
@@ -0,0 +1,27 @@
+package com.ilgul.ut3hulenko;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splashscreen extends AppCompatActivity {
+
+    private final int SPLASH_DISPLAY_LENGTH = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run(){
+                Intent intent = new Intent(Splashscreen.this, DashboardActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
