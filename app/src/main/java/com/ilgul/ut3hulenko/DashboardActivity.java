b0a44b9903d66edae2222ac65927df21dfbb6bd8
Illia Hulenko <ihulenk1@educacion.navarra.es>
2022-01-30 15:31:51 +0100

    added MapsActivity

--- app/src/main/java/com/ilgul/ut3hulenko/DashboardActivity.java
+++ app/src/main/java/com/ilgul/ut3hulenko/DashboardActivity.java
@@ -3,9 +3,7 @@ package com.ilgul.ut3hulenko;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
